bbf9c5dc126dffc6c02100a783e2a970a8081eaa
ayat <tota.samir14@gmail.com>
2019-02-08 20:32:11 +0200

    Initial Heroku commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('blog/', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap')
- ] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+ ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
