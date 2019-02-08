bfc6492e1a2f566fa41939d579771f385d4f2c55
ayat <tota.samir14@gmail.com>
2019-02-08 17:58:20 +0200

    Initial Heroku commit

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -99,8 +99,8 @@ DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-    # }
+    #
+    #  }
 }
 
 import dj_database_url
