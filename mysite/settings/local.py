803227746decf2a993e66868ec23911582b00b08
ayat <tota.samir14@gmail.com>
2019-02-08 22:06:20 +0200

    Initial Heroku commit

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -99,11 +99,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'blog',
-        'PASSWORD':'ayatayat1414',
-    }
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
 }
 
 
